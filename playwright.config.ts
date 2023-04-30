fb278ec8f3ecf8b6192c6e76d92cbca6b41f07dd
Nenko Baldzhiev <nenko.baldzhiev@gmail.com>
2023-04-30 20:12:04 +0300

    add gh workflows

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,11 +15,10 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: 0,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'line',
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
